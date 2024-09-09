1ed62ca217513999aa548491ae5361dd6857cf4e
Harsh Kumar <harshkumarsingh54321@gmail.com>
2024-09-09 23:47:25 +0530

    home page and login part done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
 
+import React from "react";
+import Navbar from "@/components/landing/Navbar";
+import Pricing from "@/components/landing/Pricing";
+import Footer from "@/components/landing/Footer";
+import HeroSection from "@/components/landing/HeroSection";
+import { authOptions, CustomSession } from "./api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
 
-export default function App() {
+export default async function Home() {
+  const session: CustomSession | null = await getServerSession(authOptions);
   return (
-    <div>app</div>
-  )
+    <>
+      <Navbar user={session?.user} />
+
+      <HeroSection />
+
+      <Pricing user={session?.user} />
+
+      <Footer />
+    </>
+  );
 }
