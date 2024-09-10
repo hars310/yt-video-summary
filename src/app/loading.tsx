bb33438ea2038f55bee4cf5e8ff885f0b75f6736
Harsh Kumar <harshkumarsingh54321@gmail.com>
2024-09-10 19:22:40 +0530

    some ui components done

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+
+export default function loading() {
+  return (
+    <div className="flex justify-center items-center my-5">
+      <div className="animate-spin w-10 h-10 border-t-4 border-b-4  border-indigo-500 rounded-full"></div>
+    </div>
+  );
+}
--- src/components/dashboard/OldSummaryCard.tsx
+++ src/components/dashboard/OldSummaryCard.tsx
@@ -1,8 +1,21 @@
+"use client";
+import Link from "next/link";
+import React from "react";
 
-const OldSummaryCard = () => {
+export default function OldSummaryCard({
+  summary,
+}: {
+  summary: UserSummaries;
+}) {
   return (
-    <div>OldSummaryCard</div>
-  )
+    <div className="bg-white shadow-md rounded-md p-4 h-52">
+      <h1 className="text-lg font-semibold">{summary.title}</h1>
+      <Link href={`/summarize?id=${summary.id}`}>
+        <h1 className="">{summary.url}</h1>
+      </Link>
+      <p className="mt-2">
+        Created At :- {new Date(summary.created_at).toDateString()}
+      </p>
+    </div>
+  );
 }
-
-export default OldSummaryCard
